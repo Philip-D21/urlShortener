5b34353883d728a83c14d08f05ad0b3009d57597
Philip-D21 <philiplekan88@gmail.com>
2023-07-05 22:19:12 +0100

    test

--- Test/auth.test.js
+++ Test/auth.test.js
@@ -1,33 +1,59 @@
-const { register } = require('../controller/auth');
+const request = require('supertest')
+const { connect } = require('./database')
+const UserModel = require('../models/userModel')
+const app = require('../index');
 
-describe('User Controller', () => {
-  describe('register', () => {
-    it('should create a new user', async () => {
-      // Mock request and response objects
-      const req = {
-        body: {
-          username: 'testuser',
-          email: 'test@example.com',
-          password: 'testpassword',
-        },
-      };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
+describe('Auth: Signup', () => {
+    let conn;
 
-      // Call the register function
-      await register(req, res);
+    beforeAll(async () => {
+        conn = await connect()
+    })
 
-      // Verify the response
-      expect(res.status).toHaveBeenCalledWith(201);
-      expect(res.json).toHaveBeenCalledWith({
-        status: 'success',
-        data: expect.objectContaining({
-          username: 'testuser',
-          email: 'test@example.com',
-        }),
-      });
-    });
-  });
-});
+    afterEach(async () => {
+        await conn.cleanup()
+    })
+
+    afterAll(async () => {
+        await conn.disconnect()
+    })
+
+    it('should signup a user', async () => {
+        const response = await request(app).post('/signup')
+        .set('content-type', 'application/json')
+        .send({ 
+            username: 'tobi', 
+            password: 'Password123', 
+            firstName: 'tobie',
+            lastName: 'Augustina',
+            email: 'tobi@mail.com'
+        })
+
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty('message')
+        expect(response.body).toHaveProperty('user')
+        expect(response.body.user).toHaveProperty('username', 'tobi')
+        expect(response.body.user).toHaveProperty('firstname', 'tobie')
+        expect(response.body.user).toHaveProperty('lastname', 'Augustina')
+        expect(response.body.user).toHaveProperty('email', 'tobi@mail.com')        
+    })
+
+
+    it('should login a user', async () => {
+        // create user in out db
+        const user = await UserModel.create({ username: 'tobi', password: '123456'});
+
+        // login user
+        const response = await request(app)
+        .post('/login')
+        .set('content-type', 'application/json')
+        .send({ 
+            username: 'tobi', 
+            password: '123456'
+        });
+    
+
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('token')      
+    })
+})
