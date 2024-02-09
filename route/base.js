87b5626a25c0bbed360c72e6db062c92bb1c8c31
Philip-D21 <philiplekan88@gmail.com>
2024-02-09 18:44:59 +0100

    ...

--- route/base.js
+++ route/base.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+
 router.route("/").get((req,res) => {
     res.render("landing");
 });
-
-
-
 
 
 module.exports = router
--- route/url.js
+++ route/url.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { authenticate} = require("../middleware/authentication")
 
+
 const {
     createShortenUrl,
     handleUrlAnalytics,
 }= require("../controller/urlController")
+
 
 router.get("/shorten", async(req,res)=>{
     res.render('main')
@@ -15,9 +17,6 @@ router.post("/shorten", authenticate, createShortenUrl);
 
 
 router.get("/analytics/:shortId", authenticate, handleUrlAnalytics);
-
-
-
 
 
 module.exports = router;
